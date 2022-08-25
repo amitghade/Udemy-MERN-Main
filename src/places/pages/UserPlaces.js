10da484169df32ea58e196d44fd62b94aa147f23
Amit Ghade <ghadeamit@gmail.com>
2022-08-25 18:19:28 +0530

    Done upto section 53

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers of the world",
+    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484,
+      lng: 73.9857,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers of the world",
+    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484,
+      lng: 73.9857,
+    },
+    creator: "u2",
+  },
+];
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
