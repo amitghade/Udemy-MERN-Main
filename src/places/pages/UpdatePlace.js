1cbf66624a3569c59f5fd854e85ae46b9375c5dd
Amit Ghade <ghadeamit@gmail.com>
2022-08-30 17:58:03 +0530

    Done upto section 68

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
@@ -38,23 +38,41 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isvalid: false,
+      },
+    },
+    false
+  );
 
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
-  const [formState, inputHandler] = useForm(
-    {
-      title: {
-        value: identifiedPlace.title,
-        isValid: true,
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true,
+        },
+        description: {
+          value: identifiedPlace.description,
+          isvalid: true,
+        },
       },
-      description: {
-        value: identifiedPlace.description,
-        isvalid: true,
-      },
-    },
-    true
-  );
+      true
+    );
+    setIsLoading(false);
+  },[setFormData, identifiedPlace]);
 
   const placeUpdateSubmitHandler = (event) => {
     event.preventDefault();
@@ -69,7 +87,16 @@ const UpdatePlace = () => {
     );
   }
 
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading....</h2>
+      </div>
+    );
+  }
+
   return (
+  
     <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
         id="title"
