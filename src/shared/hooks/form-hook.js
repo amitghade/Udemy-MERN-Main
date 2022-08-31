f52dc9dda7d619260260c802ed66c89bdede670c
Amit Ghade <ghadeamit@gmail.com>
2022-08-31 12:40:42 +0530

    Done upto section 72

--- src/shared/hooks/form-hook.js
+++ src/shared/hooks/form-hook.js
@@ -5,6 +5,10 @@ const formReducer = (state, action) => {
     case "INPUT_CHANGE":
       let formIsValid = true;
       for (const inputId in state.inputs) {
+        if (!state.inputs[inputId]) {
+          continue;
+        }
+
         if (inputId === action.inputId) {
           formIsValid = formIsValid && action.isValid;
         } else {
@@ -19,11 +23,11 @@ const formReducer = (state, action) => {
         },
         isValid: formIsValid,
       };
-      case 'SET_DATA':
-        return {
-          inputs: action.inputs,
-          isValid:action.formIsValid
-        };
+    case "SET_DATA":
+      return {
+        inputs: action.inputs,
+        isValid: action.formIsValid,
+      };
 
     default:
       return state;
@@ -46,12 +50,12 @@ export const useForm = (initialInputs, initialFormValidity) => {
   }, []);
 
   const setFormData = useCallback((inputData, formValidity) => {
-    dispatch ({
-      type: 'SET_DATA',
+    dispatch({
+      type: "SET_DATA",
       inputs: inputData,
-      formIsValid: formValidity
+      formIsValid: formValidity,
     });
-  },[]);
+  }, []);
 
   return [formState, inputHandler, setFormData];
 };
